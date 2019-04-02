32a8cca5e5ed18ff3c254f46ab1adec724688aa9
Brett Jackson <brettj85@gmail.com>
2019-04-01 20:58:44 -0500

    first MVC in Class

---
+++ FirstProject/Controllers/VehicleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            var car = new Car();
+            car.Vin = 123456;
+            car.Color = "Red";
+            return View(car); //views/vehicle/index <----- Car (model)
+        }
+    }
+    //model
+    public class Car
+    {
+        public int Vin { get; set; }
+        public string Color { get; set; }
+    }
+}
